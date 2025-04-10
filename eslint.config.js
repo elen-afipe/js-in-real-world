80438a61c94d59a89e7252e6b65d6ec3f07a37af
Lena <190838117+elen-afipe@users.noreply.github.com>
2025-04-10 20:56:53 +0300

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+
+export default defineConfig([
+	{
+		files: ["**/*.js"],
+		plugins: {
+			js,
+		},
+		extends: ["js/recommended"],
+		rules: {
+			"no-unused-vars": "warn",
+			"no-undef": "warn",
+		},
+        ignores: ["dist/", "node_modules/", "build/"]
+	},
+    eslintConfigPrettier,
+]);
+
